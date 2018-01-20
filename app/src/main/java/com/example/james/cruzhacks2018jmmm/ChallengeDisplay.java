ddb495fcdfae38e84f37bf82d62535d4d0072e4c
James Ngo <ducngocngo2@gmail.com>
2018-01-20 15:29:57 -0800

    created challengeDisplay, does nothing right now

--- app/src/main/java/com/example/james/cruzhacks2018jmmm/ChallengeDisplay.java
+++ app/src/main/java/com/example/james/cruzhacks2018jmmm/ChallengeDisplay.java
@@ -1,29 +1,57 @@
 package com.example.james.cruzhacks2018jmmm;
 
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.View;
+import android.widget.TextView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.Reader;
 
 public class ChallengeDisplay extends AppCompatActivity {
+
+    Intent intent;
+
+    String challengeFile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_display);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        TextView challengeText = (TextView) findViewById(R.id.challenge);
+        challengeText.setText(getRandChallenge(), TextView.BufferType.EDITABLE);
     }
+
+    protected void goButton(View view) {
+
+    }
+
+    protected String getRandChallenge()
+    {
+        return "hello";
+    }
+
+    protected void addChallenge()
+    {
+        FileInputStream is = null;
+        try{
+            is = openFileInput(challengeFile);
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace(System.err);
+        }
+
+
+    }
+
 
 }
--- app/src/main/java/com/example/james/cruzhacks2018jmmm/MainActivity.java
+++ app/src/main/java/com/example/james/cruzhacks2018jmmm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.james.cruzhacks2018jmmm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, ChallengeDisplay.class);
+        startActivity(intent);
     }
 }
