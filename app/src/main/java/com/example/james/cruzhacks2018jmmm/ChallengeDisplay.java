2cd7c64e6f2c3f5343f2c709fb7fc25e768c32b7
James Ngo <ducngocngo2@gmail.com>
2018-01-21 00:17:46 -0800

    This is a working database for our challenges. :D

--- app/src/main/java/com/example/james/cruzhacks2018jmmm/ChallengeDisplay.java
+++ app/src/main/java/com/example/james/cruzhacks2018jmmm/ChallengeDisplay.java
@@ -4,28 +4,25 @@ import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.JsonReader;
-import android.util.JsonToken;
-import android.util.JsonWriter;
+
 import android.view.View;
 import android.widget.TextView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
+import com.example.james.cruzhacks2018jmmm.utils.ChallengeFactory;
+import com.example.james.cruzhacks2018jmmm.utils.ChallengeList;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStreamWriter;
-import java.io.Reader;
+
 
 public class ChallengeDisplay extends AppCompatActivity {
 
     Intent intent;
 
-    String challengeFile;
+    static String challengeFile = "challenges.json";
+        ChallengeList list = new ChallengeList();
+
     String[] challenges = {"do something", "kill someone", "eat a pie", "I hate the world", "just fucking work already"};
 
 
@@ -38,6 +35,8 @@ public class ChallengeDisplay extends AppCompatActivity {
 
         TextView challengeText = (TextView) findViewById(R.id.challenge);
         challengeText.setText( getRandChallenge(), TextView.BufferType.EDITABLE);
+        createChallengeList();
+        //list.saveList();
 
 
     }
@@ -50,9 +49,27 @@ public class ChallengeDisplay extends AppCompatActivity {
 
     protected String getRandChallenge()
     {
+
         return challenges[(int) Math.floor(Math.random()*challenges.length)];
     }
 
+    protected void createChallengeList()
+    {
+
+        try{
+            FileOutputStream os = openFileOutput(challengeFile, Context.MODE_PRIVATE);
+            FileInputStream is = openFileInput(challengeFile);
+            ChallengeList list = new ChallengeList(is, os);
+            list.addChallenge(ChallengeFactory.makeChallenge("challenge", String.valueOf(Math.random()*10), "cool"));
+            System.err.println(list);
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace(System.err);
+        }
+
+
+    }
 
 
 }
---
+++ app/src/main/java/com/example/james/cruzhacks2018jmmm/utils/ChallengeList.java
@@ -0,0 +1,145 @@
+package com.example.james.cruzhacks2018jmmm.utils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Created by james on 1/20/18.
+ *
+ * Purpose: a challengelist. Recreating the wheel slightly.
+ */
+
+public class ChallengeList {
+    private JSONArray list;
+    private FileOutputStream os;
+
+    public static JSONObject challengeTemplate = null;
+    static {
+        try {
+            challengeTemplate = new JSONObject();
+            challengeTemplate.put("challenge", "");
+            challengeTemplate.put("difficulty", "0");
+            challengeTemplate.put("tags", "");
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public ChallengeList()
+    {
+        list = new JSONArray();
+    }
+
+    //if there's something here already, it isn't destructive
+    public ChallengeList(FileInputStream is, FileOutputStream os) throws IOException
+    {
+        this.os = os;
+        int size = is.available();
+        byte[] jsonStream = new byte[size];
+        is.read(jsonStream);
+        try {
+            list = new JSONArray(jsonStream);
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace(System.err);
+        }
+        System.out.println(list);
+        is.close();
+        os.close();
+
+    }
+
+    //we want to sort these challenges to get easier pulling later. It's fine for now.
+    public boolean addChallenge(JSONObject challenge)
+    {
+        if(validateChallenge(challenge))
+        {
+            list.put(challenge);
+            return true;
+        }
+        return false;
+    }
+
+    //validates that the challenge being added is legit
+    public boolean validateChallenge(JSONObject challenge)
+    {
+
+        Iterator<String> itr = challengeTemplate.keys();
+        while(itr.hasNext())
+        {
+
+            String key = itr.next();
+            if(!challenge.has(key))
+            {
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
+    //filters the challenges based on if they have a specific key, value pair
+    public ChallengeList filterChallengesByString(String attribute, String value)
+    {
+        ChallengeList retVal = new ChallengeList();
+
+        if(!challengeTemplate.has(attribute)) return retVal;
+
+        for(int i = 0; i < list.length(); i++)
+        {
+            try {
+                JSONObject obj = list.getJSONObject(i);
+                if (obj.get(attribute).equals(value)) retVal.list.put(obj);
+            }
+            catch(JSONException e)
+            {
+                e.printStackTrace(System.err);
+            }
+        }
+        return retVal;
+    }
+
+    //wrapper around list.get, can return null tho :P
+    public JSONObject get(int index)
+    {
+        try {
+            return (JSONObject) list.get(index);
+        }
+        catch(JSONException e)
+        {
+            e.printStackTrace(System.err);
+        }
+        return null;
+    }
+
+    //will save our list to the mem, testing stuff rn
+    public void saveList() {
+        try{
+            os.flush();
+            os.write(list.toString().getBytes());
+            os.close();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    public String toString()
+    {
+        return list.toString();
+    }
+
+
+
+}
