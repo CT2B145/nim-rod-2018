6df59ee9c66e4382c36dc5136623355683de1a84
James Ngo <ducngocngo2@gmail.com>
2018-01-20 19:04:45 -0800

    adding challenge display now works

--- app/src/main/java/com/example/james/cruzhacks2018jmmm/ChallengeDisplay.java
+++ app/src/main/java/com/example/james/cruzhacks2018jmmm/ChallengeDisplay.java
@@ -5,13 +5,20 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonReader;
+import android.util.JsonToken;
+import android.util.JsonWriter;
 import android.view.View;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.io.Reader;
 
 public class ChallengeDisplay extends AppCompatActivity {
@@ -19,6 +26,7 @@ public class ChallengeDisplay extends AppCompatActivity {
     Intent intent;
 
     String challengeFile;
+    String[] challenges = {"do something", "kill someone", "eat a pie", "I hate the world", "just fucking work already"};
 
 
     @Override
@@ -26,32 +34,25 @@ public class ChallengeDisplay extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge_display);
 
+        intent = getIntent();
+
         TextView challengeText = (TextView) findViewById(R.id.challenge);
-        challengeText.setText(getRandChallenge(), TextView.BufferType.EDITABLE);
+        challengeText.setText( getRandChallenge(), TextView.BufferType.EDITABLE);
+
+
     }
 
     protected void goButton(View view) {
 
+        Intent intent = new Intent(this, ChallengeSelector.class);
+        startActivity(intent);
     }
 
     protected String getRandChallenge()
     {
-        return "hello";
+        return challenges[(int) Math.floor(Math.random()*challenges.length)];
     }
 
-    protected void addChallenge()
-    {
-        FileInputStream is = null;
-        try{
-            is = openFileInput(challengeFile);
-        }
-        catch(FileNotFoundException e)
-        {
-            e.printStackTrace(System.err);
-        }
-
-
-    }
 
 
 }
